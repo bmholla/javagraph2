f92cf1bc0f02933fc886fd707587cd9ad252f8e6
Brian Holland <bholland@majordomo.local>
2017-11-20 11:22:14 -0700

    initial

---
+++ javagraph2.java
@@ -0,0 +1,7 @@
+class javagraph2
+{
+    public static void main(String args[])
+    {
+        System.out.println("hello world!");
+    }
+}
---
+++ makefile
@@ -0,0 +1,7 @@
+makajavagraph2: javagraph2.java
+	javac javagraph2.java
+
+.PHONY: clean
+
+clean:
+	rm -f *.class
